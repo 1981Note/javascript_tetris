b435350c396d781fe0557e13951715f4f0e3243c
1981Note <1760427753@qq.com>
2022-06-16 21:45:23 -0300

    fix the issue

--- app.js
+++ app.js
@@ -59,7 +59,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let random = Math.floor(Math.random() * theTetrominoes.length)
   console.log(random)
 
-  let current = theTetrominoes[0][0]
+  // let current = theTetrominoes[0][0]
+  let current = theTetrominoes[random][currentRotation]
 
   // console.log(theTetrominoes);
 
@@ -73,6 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function undraw () {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino')
+      squares[currentPosition + index].style.backgroundColor = ''
     })
   }
 
@@ -145,6 +147,30 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  // FIX ROTATION OF TETROMINOS A THE EDGE
+  function isAtRight () {
+    return current.some(index => (currentPosition + index + 1) % width === 0)
+  }
+
+  function isAtLeft () {
+    return current.some(index => (currentPosition + index) % width === 0)
+  }
+
+  function checkRotatedPosition (P) {
+    P = P || currentPosition// get current position.  Then, check if the piece is near the left side.
+    if ((P + 1) % width < 4) { // add 1 because the position index can be 1 less than where the piece is (with how they are indexed).
+      if (isAtRight()) { // use actual position to check if it's flipped over to right side
+        currentPosition += 1 // if so, add one to wrap it back around
+        checkRotatedPosition(P) // check again.  Pass position from start, since long block might need to move more.
+      }
+    } else if (P % width > 5) {
+      if (isAtLeft()) {
+        currentPosition -= 1
+        checkRotatedPosition(P)
+      }
+    }
+  }
+
   // rotate the tetromino
   function rotate () {
     undraw()
@@ -153,6 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentRotation = 0
     }
     current = theTetrominoes[random][currentRotation]
+    checkRotatedPosition()
     draw()
   }
 
